3c271e8c437df2b0bd524354fa0aad6b1ba56b32
Brian Kaemingk <brian.e.k@gmail.com>
2023-01-15 09:11:37 -0800

    Remove tmp workpath

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Fail on errors.
 set -e
+set +x
 
 # Make sure .bashrc is sourced
 . /root/.bashrc
@@ -57,8 +58,8 @@ fi # [ -f $5 ]
 if [[ -d $SPEC_FILE ]]
 then
    for i in $SPEC_FILE/*.spec; do
-       echo pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $i 
-       pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $i 
+       echo pyinstaller --clean -y --dist ./dist/windows $i 
+       pyinstaller --clean -y --dist ./dist/windows $i 
    done    
 else
    pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
